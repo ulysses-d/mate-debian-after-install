bd754119e3af5d7bb7531d883530052fbe1f8143
Dmitry Mandryk <dmandryk@gmail.com>
2015-05-27 12:28:22 +0300

    Update after-instal.sh

--- after-instal.sh
+++ after-instal.sh
@@ -1,67 +1,60 @@
 #!/bin/sh
+cd $HOME
 # Скрипт по установке среды Mate и дополнительного программного
-# обеспечения в свежеустановленный Debian 8
-# Копируем заранее подготовленный файл sources.list с dropbox
-wget -O sources.list http://goo.gl/nFmLwS
-# копируем наш файл sources.list в каталог /etc/apt/sources.list.d/
-cp sources.list /etc/apt/sources.list.d/
+# обеспечения в свежеустановленный Debian Jessie
+# Создаем sources_after_mate.list и добавляем в него дополнительные репозитории
+# Кодеки и прочая
+echo 'deb http://www.deb-multimedia.org/ jessie main non-free'  >> /etc/apt/sources.list.d/sources_after_mate.list
+# Здесь есть некоторые полезные утилиты, а также FireFox это репозиторий LMDE2
+echo 'deb http://packages.linuxmint.com/ debian main upstream import'  >> /etc/apt/sources.list.d/sources_after_mate.list
+# TLP Исключительно полезно для нетбуков и ноутбуков (в Stertch и Sid уже находится в основных репозиториях)
+echo 'deb http://repo.linrunner.de/debian jessie main'  >> /etc/apt/sources.list.d/sources_after_mate.list
+# На этом этапе еще неплохобы проверить оригинальный sources.list дописать в некоторых строчках non-free contrib но я вот пока неготов сказать как это правильно сделать, предлагаю вот такое решение (могут возникнуть дубликаты)
+echo 'deb http://ftp.ru.debian.org/debian/ jessie non-free contrib'  >> /etc/apt/sources.list.d/sources_after_mate.list
+echo 'deb http://security.debian.org/ jessie/updates contrib non-free'  >> /etc/apt/sources.list.d/sources_after_mate.list
+echo 'deb http://ftp.ru.debian.org/debian/ jessie-updates contrib non-free'  >> /etc/apt/sources.list.d/sources_after_mate.list
+echo 'deb http://ftp.ru.debian.org/debian/ jessie-backports contrib non-free'  >> /etc/apt/sources.list.d/sources_after_mate.list
 # Обновляем кэш, в результате работы команды вылезет несколько
 # строк с ошибками о том что нет доверительных ключей
 apt-get update
 # Копируем и устанавливаем ключи
-apt-get install deb-multimedia-keyring
-apt-get install debian-keyring
-wget -O- -q http://mozilla.debian.net/archive.asc | gpg --import
-gpg --check-sigs --fingerprint --keyring /usr/share/keyrings/debian-keyring.gpg 06C4AE2A
-gpg --export -a 06C4AE2A | apt-key add -
+apt-get -y install deb-multimedia-keyring linuxmint-keyring
+apt-key adv --keyserver pool.sks-keyservers.net --recv-keys CD4E8809
 # и опять обновим кэш 
-apt-get update
-# Теперь вспомним про наш Broadcom, установим его и активуруем 
-apt-get install linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') broadcom-sta-dkms
-modprobe -r b44 b43 b43legacy ssb brcmsmac
-modprobe wl
-# Перед установкой Mate полностью обновим систему
-apt-get upgrade
-# Устанавливаем основные пакеты (полезет много зависимостей, соглашаемся,
-# т.к. это практически все необходимый софт)
-apt-get install xorg lightdm xdg-user-dirs xdg-user-dirs-gtk xdg-utils pulseaudio pavucontrol mate-desktop-environment-extra network-manager network-manager-gnome iceweasel mc iceweasel-l10n-ru flashplugin-nonfree mplayer2 smplayer gksu unrar
-# Устанавливаем пакеты из Backports (полезет много зависимостей, соглашаемся,
-# т.к. это практически все необходимый софт)
-apt-get -t wheezy-backports install grub lightdm synaptic libreoffice libreoffice-l10n-ru ttf-mscorefonts-installer ttf-liberation libreoffice-style-sifr libreoffice-gtk libreoffice-gnome qt4-qtconfig transmission
-# Устанавливаем строку быстрого поиска Synaptic
-apt-get -t wheezy-backports install synaptic apt-xapian-index
-# Ставим некоторые инструменты Gnome 3 без установки рекомендованых пакетов
-apt-get install gnome-system-tools --no-install-recommends
-# Скачиваем и устанавлимаем Skype
+apt-get update; apt-get upgrade
+# Далее все разбито на отдельные команды, дабы было понимание что и зачем ставится, это можно все объеденить в одну команду
+# Iceweasel + Flash
+apt-get -y install flashplugin-nonfree
+# Libre Office -  шрифты, внешний вид, тема значков
+apt-get -y install ttf-mscorefonts-installer ttf-liberation libreoffice-style-sifr libreoffice-gtk libreoffice-gnome
+# Mate Tools
+apt-get -y install mate-system-tools caja-extensions-common caja-gksu caja-open-terminal caja-sendto dconf-editor dconf-tools mate-gnome-main-menu-applet mate-netbook mate-netspeed
+# System Tools
+apt-get -y install mc gksu unrar ntp gdebi bleachbit
+# Устанавливаем TLP
+apt-get -y install tlp tlp-rdw
+# Launchpad PPA (позволяет их использовать)
+apt-get -y install software-properties-common python-software-properties
+# Устанавливаем строку быстрого поиска Synaptic и ускоряет поиск (на совсем слабых машинах лучше не устанавливать)
+apt-get -y install synaptic apt-xapian-index
+# Почтовик Geary (Здесь можно построить меню с возможностью выбора из нескольких вариантов)
+apt-get -y install geary
+# Видео и музыка (Здесь можно построить меню с возможностью выбора из нескольких вариантов)
+apt-get -y install vlc rhythmbox
+# Создание загрузочной флешки и ее форматирование (части от LMDE2)
+apt-get -y install mintstick
+# Скачиваем mate-menu и mate-tweak из репозиториев Streatch, они не зависят от архитектуры (если кто знает как скачать deb без привязки к номеру версии, за подсказку буду благодарен)
+wget http://ftp.ru.debian.org/debian/pool/main/m/mate-menu/mate-menu_5.6.5a-1_all.deb
+wget http://ftp.ru.debian.org/debian/pool/main/m/mate-tweak/mate-tweak_3.4.9-1_all.deb
+# конфигурация внешнего вида программ Qt (не очень актуально, т.к. и VLC и Skype настраиваются нормально уже своими штатными средствами через выбор средства отображения GTK+)
+apt-get -y install qt4-qtconfig
+# Скачиваем Skype
 wget -O skype-install.deb http://www.skype.com/go/getskype-linux-deb
-dpkg -i skype-install.deb
-# Скачиваем и устанавливаем утилиту по настройке lightdm
-wget -O lightdmcfg_0.1.1-2_all.deb http://goo.gl/LNN1zu
-dpkg -i lightdmcfg_0.1.1-2_all.deb
-# Удовлетворяем зависимости после Skype и lightdmcfg
+# Устанавливаем скачанные пакеты и удовлетворяем зависимости
+dpkg -i *.deb
 apt-get -f install
-# ----------------
-# Не обязательный раздел - Работа напильником
-# Перемещаем кнопки влево
-dconf write /org/mate/marco/general/button-layout "'close,minimize,maximize:'"
-# Сворочиваем/Разворачиваем все окна по комбинации клавишь <Win>+<d>
-dconf write /org/mate/marco/global-keybindings/show-desktop "'<Mod4>d'"
-# Запускаем терминал стандартной командой <Control>+<Alt>+<t> - по умолчанию ваще ничего не стоит
-dconf write /org/mate/marco/global-keybindings/run-command-terminal "'<Control><Alt>t'"
-# Разбираемся со шрифтами
-dconf write /org/mate/interface/document-font-name "'Droid Sans 10'"
-dconf write /org/mate/interface/font-name "'Droid Sans 10'"
-dconf write /org/mate/interface/monospace-font-name "'Droid Sans Mono 10'"
-dconf write /org/mate/marco/general/titlebar-font "'Droid Sans Bold 10'"
-# Ставим свою тему окон
-dconf write /org/mate/marco/general/theme "'BlueMenta'"
-dconf write /org/mate/interface/gtk-theme "'BlueMenta'"
-# Ставим свои иконки
-dconf write /org/mate/interface/icon-theme "'matefaenza'"
-# ----------------
-echo "ПОЗДРАВЛЯЮ!"
-echo "установка системы закончены"
-echo "теперь необходимо выйти из сеанса root"
-echo "командой exit"
-echo "и перегрузить компьютер"
-echo "можно даже c помощью Ctrl+Alt+Del"
+# Удаляем ненужное (ИМХО)
+apt-get purge gnome-orca
+# Чистим после себя 
+apt-get autoremove; apt-get clean; apt-get autoclean
+
